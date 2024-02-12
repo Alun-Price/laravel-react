3026a8b069175cdcf573e7a7d5e11318428b7950
Alun Price <alunprice089@gmail.com>
2024-02-12 19:50:27 +0000

    Created protected routes

--- vite-project/src/components/DefaultLayout.jsx
+++ vite-project/src/components/DefaultLayout.jsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+import { useEffect } from "react";
 
 export default function DefaultLayout() {
+    const { user, token, setUser, setToken } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
+
     return (
         <div>
             Default
